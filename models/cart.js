e75b3f19feb6748f0ab20a069c8b323e35566f6e
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-05-29 08:20:35 -0500

    first commit

---
+++ models/cart.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require ('../util/database');
+
+const Cart = sequelize.define('Cart',{
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+
+  }
+});
+
+module.exports = Cart;
